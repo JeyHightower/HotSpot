9322b00ab9c0b5c30de47ca3ccffd8c420c2f2bf
JeyHightower <Jamesehightower@gmail.com>
2024-10-14 06:53:46 -0500

    finished profile button

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,1 +1,54 @@
 import { FaUserCheck } from 'react-icons/fa6';
+import { useDispatch } from 'react-redux';
+import { useState, useEffect, useRef } from 'react';
+import * as sessionActions from '../../store/session';
+
+function ProfileButton({ user }) {
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef();
+
+
+    const toggleMenu = (e) => {
+        e.stopPropagation();
+        setShowMenu(!showMenu);
+    };
+
+    useEffect(() => {
+        if (!showMenu) return;
+        
+        const closeMenu  = (e) => {
+            if (ulRef.current && !ulRef.current.contains(e.target)) {
+                setShowMenu(false);
+            }
+        };
+        document.addEventListener('click', closeMenu);
+        return () => document.removeEventListener('click', closeMenu)
+    }, [showMenu]);
+
+        const logout = (e) => {
+            e.preventDefault()
+            dispatch(sessionActions.logout());
+        };
+
+        const ulClassName = "profile-dropdown" + (showMenu ? "" : "hidden");
+    return (
+        <>
+        <button onClick={() => setShowMenu(!showMenu)}>
+            <FaUserCheck />
+            </button>
+            <uL className ={ulClassName} ref={ulRef}>
+                <li>{user.username}</li>
+                <li>{user.firstName} {user.lastName}</li>
+                <li>{user.email}</li>
+                <li>
+                    <button onClick={logout}>Log Out</button>
+                </li>
+
+            </uL>
+            
+            </>
+    );
+}
+
+export default ProfileButton;
