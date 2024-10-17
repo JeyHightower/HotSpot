1761386d8ec1b50c1aa528ad4b68cedb42f8ca3c
JeyHightower <Jamesehightower@gmail.com>
2024-10-17 10:15:29 -0500

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import {
 } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
 import * as sessionActions from "./store/session";
-import "./App.css";
+import "./index.css";
 
 function Layout() {
   const dispatch = useDispatch();
